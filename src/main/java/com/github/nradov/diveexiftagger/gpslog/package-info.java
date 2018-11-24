7e5ef0cd22d77a91cb12f56aa065eaec5f62806c
Nick Radov <nradov@gmail.com>
2018-11-23 22:08:54 -0800

    ExifTool

--- src/main/java/com/github/nradov/diveexiftagger/gpslog/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Sources of GPS log data. The data points in these logs can be used to
- * retroactively add GPS tags to images. If your log format isn't supported then
- * you might be able to convert it with
- * <a target="_" href="https://www.gpsbabel.org/">GPSBabel</a>.
- *
- * @author Nick Radov
- */
-package com.github.nradov.diveexiftagger.gpslog;
--- src/main/java/com/github/nradov/diveexiftagger/image/jpeg/exif/AbstractImageFileDirectory.java
+++
@@ -1,5 +0,0 @@
-package com.github.nradov.diveexiftagger.image.jpeg.exif;
-
-abstract class AbstractImageFileDirectory {
-
-}
